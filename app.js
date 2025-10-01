5bf58f448dec5a96643fd251910d4a24b7c42948
itssonya045 <shrirakshe2004@email.com>
2025-10-01 15:58:53 +0530

    Connecting the Database

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const  {ConnectDB} = require("./src/config/database"); 
 const app = express();
 
+ ConnectDB()
+.then(() => {
+    console.log("Database connection successfully.");
 
-app.listen("7777",(req,res)=>{
-    console.log("server working well.")
+    app.listen(7777, () => {
+        console.log("Server working well on port 7777.");
+    });
 })
+.catch((err) => {
+    console.log("Database connection unsuccessfully.", err);
+});
+
