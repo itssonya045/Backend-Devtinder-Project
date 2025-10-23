b0ab531bf39a4ac3e5d13d8d3391004d1a914a36
itssonya045 <shrirakshe2004@email.com>
2025-10-23 05:55:26 +0530

    fetching connections

--- src/Routers/userRouter.js
+++ src/Routers/userRouter.js
@@ -21,5 +21,36 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/connections",userAuth , async(req,res)=>{
 
+
+  try{
+    const loggedUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or :[
+        {
+        toUserId: loggedUser._id , status : "accepted",
+        },
+       {
+         fromUserId : loggedUser._id , status : "accepted"
+      }]
+  }).populate("fromUserId", ["firstName", "lastName" , "photoUrl"])
+  .populate("toUserId", ["firstName", "lastName" , "photoUrl"])
+
+
+  const data = connectionRequest.map((k) =>
+  k.toUserId._id.toString() === loggedUser._id.toString()
+    ? k.fromUserId
+    : k.toUserId
+);
+
+
+
+    res.json({data })
+
+  }catch(err){
+    res.status(400).send("ERROR: " + err.message);
+  }
+})
 module.exports = userRouter
