7d7c1d3396532e756c5f1c6606c6767dd843b2ee
itssonya045 <shrirakshe2004@email.com>
2025-10-27 10:44:01 +0530

    pagination for feed api

--- src/Routers/userRouter.js
+++ src/Routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionReq");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 
@@ -46,11 +47,57 @@ userRouter.get("/user/connections",userAuth , async(req,res)=>{
 );
 
 
-
     res.json({data })
 
   }catch(err){
     res.status(400).send("ERROR: " + err.message);
   }
 })
+
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedUser = req.user;
+
+    const page = parseInt(req.query.page)||1;
+    let limit = parseInt(req.query.limit)||10
+    limit = limit > 50 ? 50 : limit
+    const skip = (page - 1) * limit;
+
+
+    // 1️⃣ Find all connection requests involving the logged user
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedUser._id },
+        { toUserId: loggedUser._id }
+      ]
+    })
+      .select("fromUserId toUserId")
+      .populate("fromUserId", ["firstName"])
+      .populate("toUserId", ["firstName"]);
+
+    // 2️⃣ Create a set of user IDs to hide
+    const hideFeed = new Set();
+
+    connectionRequest.forEach((request) => {
+      hideFeed.add(request.fromUserId._id.toString());
+      hideFeed.add(request.toUserId._id.toString());
+    });
+
+    // 3️⃣ Find all users who are NOT connected and NOT the logged user
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideFeed) } },
+        { _id: { $ne: loggedUser._id } }
+      ]
+    }).select("firstName lastName email").skip(skip).limit(limit)
+
+    // 4️⃣ Send the feed
+    res.send(users);
+
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+});
+
 module.exports = userRouter
